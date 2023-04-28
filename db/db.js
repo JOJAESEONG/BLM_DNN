c3356c0293dc7853172a83625a6633b36a92243b
jaeseong <pidoco40@gmail.com>
2023-04-28 10:21:52 +0900

    db.js crateConnection -> createPool

--- db/db.js
+++ db/db.js
@@ -1,10 +1,12 @@
 var mysql = require('mysql2');
-var db = mysql.createConnection({
+var db = mysql.createPool({
     host: 'mlsensor.cdpjszvvkbxz.ap-northeast-2.rds.amazonaws.com',
     user: 'ehrnc',
     password: 'ehrnc6458!',
-    database: 'login_test'
+    database: 'login_test',
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+    idleTimeout: 60000 
 });
-db.connect();
-
 module.exports = db;
